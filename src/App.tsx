3458308e75fcf95acf45055b1032da7f4c874f7a
iewher <goshagang@mail.ru>
2023-12-07 21:52:28 +0500

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./scss/app/app.scss";
+
+function App() {
+  return (
+    <div className="App">
+      <p>all-terrain-vehicles</p>
+    </div>
+  );
+}
+
+export default App;
